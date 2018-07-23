bc28f4a233483702cb8e017204d0485d9af7ca1e
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-07-23 16:22:06 -0300

    mapping class -> database

--- ContentManage/src/AppBundle/Controller/UserController.php
+++ ContentManage/src/AppBundle/Controller/UserController.php
@@ -2,8 +2,11 @@
 // src/AppBundle/Controller/UserController.php
 namespace AppBundle\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Entity\User;
 
 
 class UserController extends Controller
@@ -11,8 +14,12 @@ class UserController extends Controller
     /**
      * @Route("/validation")
      */
-    public function validaAction()
+    public function validaAction(Request $request)
     {
-        return new Response('test');
+      // $email = $request->query->get('email'); via get
+      $email = $request->request->get('email');
+      $password = $request->request->get('password');
+      $msg = "I had received the email: " . $email . " and a password: " . $password;
+      return new Response($msg);
     }
 }
