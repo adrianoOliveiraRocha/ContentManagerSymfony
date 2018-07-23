ed63c1c93a771d3f15a10e43b757cb7e1aefbfd8
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-07-23 13:32:29 -0300

    organize pages

--- ContentManage/src/AppBundle/Controller/DefaultController.php
+++ ContentManage/src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,24 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/sobre", name="about")
+     */
+    public function aboutAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/about.html.twig', []);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/contact.html.twig', []);
     }
 }
