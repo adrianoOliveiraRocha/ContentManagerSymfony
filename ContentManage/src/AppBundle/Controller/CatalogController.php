a94c32aa3eab09f9707fd49708fa3890e8173075
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-07-25 08:36:48 -0300

    edit categor

--- ContentManage/src/AppBundle/Controller/CatalogController.php
+++ ContentManage/src/AppBundle/Controller/CatalogController.php
@@ -46,6 +46,41 @@ class CatalogController extends Controller{
       'categories' => $categories,
     ));
   }
+
+  /**
+   * @Route("/edit_category", name="edit_category")
+   */
+  public function editCategoryAction(Request $request){
+    $id = $request->query->get('id');
+    $name = $request->query->get('name');
+
+    return $this->render('catalog/edit_category.html.twig', array(
+      'id' => $id,
+      'name' => $name,
+    ));
+  }
+
+  /**
+   * @Route("/run_edit_category", name="run_edit_category")
+   */
+  public function runEditCategoryAction(Request $request){
+    $id = $request->request->get('id');
+    $name = $request->request->get('name');
+
+    $repository = $this->getDoctrine()->getRepository(Category::class);
+    $category = $repository->findOneById($id);
+    $category->setName($name);
+
+    try {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($category);
+      $entityManager->flush();
+      $this->addFlash('msg', 'Categoria editada com sucesso!');
+      return $this->render('accounts/admin.html.twig');
+    } catch (\Exception $e) {
+      return new Response($e->getMessage());
+    }
+  }
 }
 
 
